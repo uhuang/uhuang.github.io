1ff02f408efbbefbecabbbcc14337411c39670ce
uhuang <uhuang@users.noreply.github.com>
2019-07-11 00:24:37 -0400

    COLLAPSING BUTTONS!

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,5 +92,20 @@
 			});
 
 		}
+	// Accordion (with thanks to w3schools.com)
+		var acc = document.getElementsByClassName("accordion");
+		var i;
+
+		for (i = 0; i < acc.length; i++) {
+		  acc[i].addEventListener("click", function() {
+		    this.classList.toggle("active");
+		    var panel = this.nextElementSibling;
+		    if (panel.style.maxHeight){
+		      panel.style.maxHeight = null;
+		    } else {
+		      panel.style.maxHeight = panel.scrollHeight + "px";
+		    }
+		  });
+		}
 
 })(jQuery);
